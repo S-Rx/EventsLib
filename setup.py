58e22a33e14b836008b012393f425329336209b8
S-Rx <git.rx@yandex.ru>
2014-07-07 23:10:14 +0400

    added comments

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='rx_events',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='redlex',
+    author_email='',
+    description='simple  events and delegates implementation'
+)
